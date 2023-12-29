7f53ca048e9292844fc5a6f6931112d47875ba32
anakin-dabir <anakindabir@gmail.com>
2023-12-29 20:27:42 +0500

    done again

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <div className='w-screen h-72 md:h-[600px] lg:h-[700px] relative'>
-        <Image
-          src='/bg.png'
-          height={1000000}
-          width={1000000}
-          priority
-          className='w-screen h-72 md:h-[600px] lg:h-[700px] object-cover object-center'
-        />
+      <div className='w-screen h-72 md:h-[600px] lg:h-[calc(100vh-150px)] -mt-1 relative'>
+        <Image src='/bg.png' fill={true} priority className='w-screen object-cover object-center' />
         <div className='flex flex-col gap-2 pr-2 lg:pr-10 w-full absolute top-1/3 -translate-y-1/3'>
           <div className='font-chakra text-right  text-white font-bold text-xl md:text-5xl italic drop-shadow-_text'>
             <div>PNEUS</div>
